6ac145ffbaa22fe686c94f7833102a3d1ffd06f5
Daniele Tabanella <daniele.tabanella@gmail.com>
2021-07-15 21:51:43 +0200

    scale fireflies

--- src/engines/Three/Three-Entity.ts
+++ src/engines/Three/Three-Entity.ts
@@ -2,19 +2,40 @@
 import Sketch from './Three';
 
 export default abstract class Entity {
-
-  abstract update(elapsedTime: number): void;
+/**
+ * This function is called by the sketch at every requestAnimationFrame.
+ *
+ * @abstract
+ * @param {number} elapsedTime
+ * @memberof Entity
+ */
+abstract update(elapsedTime: number): void;
   private sketch: Sketch;
+/**
+ * This function is called by the sketch once the entity is added to it.
+ *
+ * @abstract
+ * @memberof Entity
+ */
+abstract init(): void;
 
-  abstract init(): void;
-
-  setSketch(sketch: Sketch): void {
-    this.sketch = sketch;
-  }
-
-  getSketch(): Sketch {
-    return this.sketch;
-  }
-
-  abstract destroy(): void;
+setSketch(sketch: Sketch): void {
+  this.sketch = sketch;
+}
+/**
+ * This function returns the parent sketch and its members (camera, scene, etc)
+ *
+ * @return {*}  {Sketch}
+ * @memberof Entity
+ */
+getSketch(): Sketch {
+  return this.sketch;
+}
+/**
+ * This function is called by the sketch when the entity is removed from it.
+ *
+ * @abstract
+ * @memberof Entity
+ */
+abstract destroy(): void;
 }
--- src/shaders/fireflies/vertex.glsl
+++ src/shaders/fireflies/vertex.glsl
@@ -6,7 +6,9 @@ attribute float aScale;
 
 void main()
 {
+
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    float scale = cos(uTime * modelPosition.y * modelPosition.x) * aScale;
     modelPosition.x += cos(uTime * (1.0 / aScale) / 2.0 + modelPosition.x * 100.0) * aScale * 0.2;
     modelPosition.y += sin(uTime * (1.0 / aScale) / 2.0 + modelPosition.y * 100.0) * aScale * 0.3;
     modelPosition.z += sin(uTime * (1.0 / aScale) / 2.0 + modelPosition.z * 100.0) * aScale * 0.2;
@@ -16,6 +18,6 @@ void main()
 
     gl_Position = projectionPosition;
 
-    gl_PointSize = uSize * aScale * uPixelRatio;
+    gl_PointSize = uSize * scale * uPixelRatio;
     gl_PointSize *= (1.0 / - viewPosition.z);
 }
